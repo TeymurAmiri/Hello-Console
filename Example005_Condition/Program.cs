4b4bffebed56a376e2f5d0753950ab3fc55fe4cd
Teymur <timuramiri@yandex.ru>
2022-08-03 19:51:26 +0300

    Example005 Condition Project

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "оля")
+{
+    Console.Write("Ура, это же Оля!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username + "!");
+}
