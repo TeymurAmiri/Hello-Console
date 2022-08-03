eb81879c334f331fe38a4d99c71e2a876469b6ef
Teymur <timuramiri@yandex.ru>
2022-08-03 19:36:37 +0300

    Example003 sum with Random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 10;
+﻿int numberA = new Random().Next(1,100); //Случайное число от 1 до 100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100); //Случайное число от 1 до 100
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
