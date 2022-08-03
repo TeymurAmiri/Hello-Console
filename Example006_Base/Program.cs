ebff83facbf13a5c3b710e41f07c7e69f153e8a4
Teymur <timuramiri@yandex.ru>
2022-08-03 20:01:13 +0300

    Example006_Base Project

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 10;
+int b = 21;
+int c = 16;
+int d = 22;
+int e = 42;
+int max = a;
+
+if( a > max ) max = a;
+if( b > max ) max = b;
+if( c > max ) max = c;
+if( d > max ) max = d;
+if( e > max ) max = e;
+Console.Write("Максимальное значение: ");
+Console.Write(max);
